b3a017d2273ad9cbf68dc59e5a94d777caeef4b4
Ana K <@anakodeq>
2023-07-25 21:34:51 +0300

    Example 005 Condition added

---
+++ Ex005_Condition/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write ("Write your name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower == "huckleberry")
+{
+    Console.WriteLine("Hey, are you mine Huckleberry?");
+}
+else 
+{
+    Console.Write("Howdy, ");
+    Console.WriteLine(username);
+}
